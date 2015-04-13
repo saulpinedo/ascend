e17e51b9839df071735f325d8a5dec3ac79d846f
saul <saul@saul-desktop.(none)>
2015-04-13 15:31:33 -0400

    reparando

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  #true
+  config.serve_static_files = true
+  #ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
 
 
   # Compress JavaScripts and CSS.
@@ -31,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   #true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
